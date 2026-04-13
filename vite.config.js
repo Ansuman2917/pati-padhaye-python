0a526e53fd5f9b41eb6864d9445fc2bbe94ec886
Ansuman2917 <teamvalorage21@gmail.com>
2026-04-13 12:35:26 +0530

    Update for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  server: { host: true }
+  base: '/pati-padhaye-python/',
+  server: { host: true },
+  build: {
+    assetsDir: 'assets'
+  }
 })
